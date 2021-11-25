0b2fd68764309f8a4ba54fdb68ca064a137bd53f
Holmern <chri63dj@stud.kea.dk>
2021-11-25 14:06:20 +0100

    first commit

---
+++ Django_Neo4j/InkItUp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class InkitupConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'InkItUp'
---
+++ Django_Neo4j/InkItUp/models.py
@@ -0,0 +1,80 @@
+from neomodel import StructuredNode, StringProperty, IntegerProperty, UniqueIdProperty, RelationshipTo
+from neomodel.properties import DateProperty, FloatProperty
+from neomodel.relationship_manager import RelationshipFrom
+
+# Create your models here.
+class Tattooparlor(StructuredNode):
+    cvr = IntegerProperty(unique_index=True, required=True)
+    name = StringProperty(index=True, required=True)
+    phonenumber = IntegerProperty(index=True, required=True)
+    adress = StringProperty(index=True, required=True)
+    email = StringProperty(index=True, required=True)
+
+class Customer(StructuredNode):
+    cpr = IntegerProperty(unique_index=True, required=True)
+    name = StringProperty(index=True, required=True)
+    email = StringProperty(index=True, required=True)
+    phonenumber = IntegerProperty(index=True, required=True)
+    registered = DateProperty(required=True)
+
+class Appointment(StructuredNode):
+    idappointment = UniqueIdProperty()
+    datetime=DateProperty(required=True)
+    sessionlenght=IntegerProperty(index=True, default=1)
+
+    #Relationships
+    customer = RelationshipFrom(Customer, 'HAS_AN')
+    tatooparlor = RelationshipFrom(Tattooparlor, 'HOLDS_AN')
+     
+    
+class Artist(StructuredNode):
+    cpr = IntegerProperty(unique_index=True, required=True)
+    name = StringProperty(index=True, required=True)
+    phonenumber = IntegerProperty(index=True, required=True)
+    email = StringProperty(index=True, required=True)
+    price = IntegerProperty(index=True, required=True)
+    
+    #Relationships
+    tattooparlor = RelationshipTo(Tattooparlor, 'WORKS_IN')
+    customer = RelationshipTo(Customer, 'HAS_AN')
+
+
+class Ink(StructuredNode):
+    batchnumber = IntegerProperty(unique_index=True, required=True)
+    brand = StringProperty(index=True, required=True)
+    colorcode = StringProperty(index=True, required=True)
+    experationdate = DateProperty(required=True)
+    price = FloatProperty(index=True, required=True)
+
+    #Relationships
+    tattooparlor = RelationshipFrom(Tattooparlor, 'HAS')
+
+class Producer(StructuredNode):
+    cvr = IntegerProperty(unique_index=True, required=True)
+    name = StringProperty(index=True, required=True)
+    phonenumber = IntegerProperty(index=True, required=True)
+    adress = StringProperty(index=True, required=True)
+
+    #Relationships
+    ink = RelationshipTo(Ink, 'PRODUCES')
+
+class Supplier(StructuredNode):
+    cvr = IntegerProperty(unique_index=True, required=True)
+    name = StringProperty(index=True, required=True)
+    phonenumber = IntegerProperty(index=True, required=True)
+    adress = StringProperty(index=True, required=True)
+    
+    #Relationships
+    producer = RelationshipTo(Producer, 'HAS_A')
+
+class Tattoo(StructuredNode):
+    idtattoo = UniqueIdProperty()
+    description = StringProperty(index=True, required=True)
+    placementonbody = StringProperty(index=True, required=True)
+
+    #Relationships
+    appointment = RelationshipTo(Appointment, 'HAS_A')
+    ink = RelationshipTo(Ink, 'MADE_WITH')
+
+
+
